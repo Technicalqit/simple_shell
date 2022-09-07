2f0240e9e51775b0ba75ba89b731f40d97531175
lydiauzoma <99988048+lydiauzoma@users.noreply.github.com>
2022-09-07 21:52:36 +0100

    added changes

--- 0x02-shell_utilization.c
+++ 0x02-shell_utilization.c
@@ -1,341 +1,74 @@
 #include "shell.h"
 
-
-
 /**
-
- * Auth: Emma Udeji
-
- * 		 Pericles Adjovi
-
+ * interactive - returns true if shell is interactive mode
+ * @info: struct address
  *
-
- * Description:
-
- * the extended functions for main.c
-
+ * Return: 1 if interactive mode, 0 otherwise
  */
-
-
-
-
-
-/** parse_command - determines the type of the command
-
- * @command: command to be parsed
-
- *
-
- * Return: constant representing the type of the command
-
- * Description -
-
- * 		 EXTERNAL_COMMAND (1) represents commands like /bin/ls
-
- *		 INTERNAL_COMMAND (2) represents commands like exit, env
-
- *		 PATH_COMMAND (3) represents commands found in the PATH like ls
-
- *		 INVALID_COMMAND (-1) represents invalid commands
-
- */
-
-
-
-int parse_command(char *command)
-
+int interactive(info_t *info)
 {
-
-	int i;
-
-	char *internal_command[] = {"env", "exit", NULL};
-
-	char *path = NULL;
-
-
-
-	for (i = 0; command[i] != '\0'; i++)
-
-	{
-
-		if (command[i] == '/')
-
-			return (EXTERNAL_COMMAND);
-
-	}
-
-	for (i = 0; internal_command[i] != NULL; i++)
-
-	{
-
-		if (_strcmp(command, internal_command[i]) == 0)
-
-			return (INTERNAL_COMMAND);
-
-	}
-
-	/* @check_path - checks if a command is found in the PATH */
-
-	path = check_path(command);
-
-	if (path != NULL)
-
-	{
-
-		free(path);
-
-		return (PATH_COMMAND);
-
-	}
-
-
-
-	return (INVALID_COMMAND);
-
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
-
+/**
+ * is_delim - checks if character is a delimeter
+ * @c: the char to check
+ * @delim: the delimeter string
+ * Return: 1 if true, 0 if false
+ */
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
+}
 
 /**
-
- * execute_command - executes a command based on it's type
-
- * @tokenized_command: tokenized form of the command (ls -l == {ls, -l, NULL})
-
- * @command_type: type of the command
-
- *
-
- * Return: void
-
+ *_isalpha - checks for alphabetic character
+ *@c: The character to input
+ *Return: 1 if c is alphabetic, 0 otherwise
  */
 
-void execute_command(char **tokenized_command, int command_type)
-
+int _isalpha(int c)
 {
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
 
-	void (*func)(char **command);
+/**
+ *_atoi - converts a string to an integer
+ *@s: the string to be converted
+ *Return: 0 if no numbers in string, converted number otherwise
+ */
 
+int _atoi(char *s)
+{
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
 
-
-	if (command_type == EXTERNAL_COMMAND)
-
+	for (i = 0;  s[i] != '\0' && flag != 2; i++)
 	{
+		if (s[i] == '-')
+			sign *= -1;
 
-		if (execve(tokenized_command[0], tokenized_command, NULL) == -1)
-
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-
-			perror(_getenv("PWD"));
-
-			exit(2);
-
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
 		}
-
+		else if (flag == 1)
+			flag = 2;
 	}
 
-	if (command_type == PATH_COMMAND)
-
-	{
-
-		if (execve(check_path(tokenized_command[0]), tokenized_command, NULL) == -1)
-
-		{
-
-			perror(_getenv("PWD"));
-
-			exit(2);
-
-		}
-
-	}
-
-	if (command_type == INTERNAL_COMMAND)
-
-	{
-
-		func = get_func(tokenized_command[0]);
-
-		func(tokenized_command);
-
-	}
-
-	if (command_type == INVALID_COMMAND)
-
-	{
-
-		print(shell_name, STDERR_FILENO);
-
-		print(": 1: ", STDERR_FILENO);
-
-		print(tokenized_command[0], STDERR_FILENO);
-
-		print(": not found\n", STDERR_FILENO);
-
-		status = 127;
-
-	}
-
-}
-
-
-
-/**
-
- * check_path - checks if a command is found in the PATH
-
- * @command: command to be used
-
- *
-
- * Return: path where the command is found in, NULL if not found
-
- */
-
-char *check_path(char *command)
-
-{
-
-	char **path_array = NULL;
-
-	char *temp, *temp2, *path_cpy;
-
-	char *path = _getenv("PATH");
-
-	int i;
-
-
-
-	if (path == NULL || _strlen(path) == 0)
-
-		return (NULL);
-
-	path_cpy = malloc(sizeof(*path_cpy) * (_strlen(path) + 1));
-
-	_strcpy(path, path_cpy);
-
-	path_array = tokenizer(path_cpy, ":");
-
-	for (i = 0; path_array[i] != NULL; i++)
-
-	{
-
-		temp2 = _strcat(path_array[i], "/");
-
-		temp = _strcat(temp2, command);
-
-		if (access(temp, F_OK) == 0)
-
-		{
-
-			free(temp2);
-
-			free(path_array);
-
-			free(path_cpy);
-
-			return (temp);
-
-		}
-
-		free(temp);
-
-		free(temp2);
-
-	}
-
-	free(path_cpy);
-
-	free(path_array);
-
-	return (NULL);
-
-}
-
-
-
-/**
-
- * get_func - retrieves a function based on the command given and a mapping
-
- * @command: string to check against the mapping
-
- *
-
- * Return: pointer to the proper function, or null on fail
-
- */
-
-void (*get_func(char *command))(char **)
-
-{
-
-	int i;
-
-	function_map mapping[] = {
-
-		{"env", env}, {"exit", quit}
-
-	};
-
-
-
-	for (i = 0; i < 2; i++)
-
-	{
-
-		if (_strcmp(command, mapping[i].command_name) == 0)
-
-			return (mapping[i].func);
-
-	}
-
-	return (NULL);
-
-}
-
-
-
-/**
-
- * _getenv - gets the value of an environment variable
-
- * @name: name of the environment variable
-
- *
-
- * Return: the value of the variable as a string
-
- */
-
-char *_getenv(char *name)
-
-{
-
-	char **my_environ;
-
-	char *pair_ptr;
-
-	char *name_cpy;
-
-
-
-	for (my_environ = environ; *my_environ != NULL; my_environ++)
-
-	{
-
-		for (pair_ptr = *my_environ, name_cpy = name;
-
-		     *pair_ptr == *name_cpy; pair_ptr++, name_cpy++)
-
-		{
-
-			if (*pair_ptr == '=')
-
-				break;
-	}
-		if ((*pair_ptr == '=') && (*name_cpy == '\0'))
-		return (pair_ptr + 1);
-	}
-	return (NULL);
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+
+	return (output);
 }
